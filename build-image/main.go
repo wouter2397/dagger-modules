7c259323bd0bc33da2815e567871b9029a52cb7e
Wouter Scholte in 't Hoff <wouter@dtwm.nl>
2025-11-09 13:53:24 +0100

    Update modules to dagger 0.19.6

--- build-image/main.go
+++ build-image/main.go
@@ -1,16 +1,11 @@
 // A generated module for BuildImage functions
 //
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
+// This dagger module is created to integrate with the following dagger modules:
+// - docker
+// - trivy
+// - cosign
+// It provides a high-level function to build a Docker image, scan it for vulnerabilities,
+// push it to a registry, and sign it.
 
 package main
 
@@ -21,7 +16,10 @@ import (
 
 type BuildImage struct{}
 
-// Returns a container that echoes whatever string argument is provided
+// BuildImage builds a Docker image from the specified directory and Dockerfile,
+// scans it for vulnerabilities using Trivy, pushes it to a container registry,
+// and signs it using Cosign.
+// It returns the output of the vulnerability analysis and the image digest.
 func (m *BuildImage) BuildImage(ctx context.Context, dir *dagger.Directory, file string, imageRef string, privateKey *dagger.Secret, registryUsername string, registryPassword *dagger.Secret, password *dagger.Secret) (string, error) {
 	container := dag.Docker().DockerBuild(dir, file)
 	sbom := dag.Trivy().ScanContainer(container, imageRef)
